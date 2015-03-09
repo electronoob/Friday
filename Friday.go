ef14b3790e2149d81ce7ab49af895e3ea127f784
electronoob <github@electronoob.com>
2015-03-08 23:04:55 -0400

    updating some changes

--- Friday.go
+++ Friday.go
@@ -1,32 +1,51 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "encoding/json"
-    "log"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
+
 type Message struct {
-    Key string
-    Value string
-    Time int64
+	Key   string
+	Value string
+	Time  int64
 }
 
 func handle_slash(w http.ResponseWriter, r *http.Request) {
-    http.Redirect(w, r, "/static/", 301)
+	http.Redirect(w, r, "/static/", 301)
 }
+func handle_api_slash(w http.ResponseWriter, r *http.Request) {
+	m := Message{"_error", "Incomplete API request.", 0}
+	b, err := json.Marshal(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, "%s", b)
+}
+func handle_api_get(w http.ResponseWriter, r *http.Request) {
+}
+func handle_api_set(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() // Parses the request body
+	key := r.PostFormValue("Key")
+	value := r.PostFormValue("Value")
 
-func handle_db(w http.ResponseWriter, r *http.Request) {
- m := Message{"Alice", "Hello", 1294706395881547000}
- b, err := json.Marshal(m)
- if err != nil {
-    log.Fatal(err)
- }
- fmt.Fprintf(w, "%s", b)
+	m := Message{key, value, time.Now().UnixNano()}
+	b, err := json.Marshal(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, "%s", b)
+	// html.EscapeString()
 }
 func main() {
-    http.HandleFunc("/", handle_slash)
-    http.HandleFunc("/db/", handle_db)
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-    http.ListenAndServe("0.0.0.0:7008", nil)
+	http.HandleFunc("/", handle_slash)
+	http.HandleFunc("/api/", handle_api_slash)
+	http.HandleFunc("/api/get/", handle_api_get)
+	http.HandleFunc("/api/set/", handle_api_set)
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	log.Fatal(http.ListenAndServe("0.0.0.0:7008", nil))
 }
