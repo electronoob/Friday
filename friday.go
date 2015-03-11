dc4a221af3215bbae2e8b54fcf314798c2f58e0b
Ian Remsen <ian@ianremsen.com>
2015-03-10 21:01:17 -0400

    Update and rename Friday.go to friday.go

---
+++ friday.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"friday/api"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("/var/www/friday/")))
+	http.HandleFunc("/api/", api.HandleApiSlash)
+	http.HandleFunc("/api/get/", api.HandleApiGet)
+	http.HandleFunc("/api/set/", api.HandleApiSet)
+	log.Fatal(http.ListenAndServe("0.0.0.0:7008", nil))
+}
