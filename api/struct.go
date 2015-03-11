e2e067abc53be5db6c2576f97928bb5c7947ab82
Ian Remsen <ian@ianremsen.com>
2015-03-10 21:02:15 -0400

    Create struct.go

---
+++ api/struct.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+    "github.com/pmylund/go-cache"
+    "time"
+)
+
+type Message struct {
+    Key   string
+    Value string
+    Time  int64
+}
+
+var c = cache.New(5*time.Hour, 1*time.Hour)
+var i int = 0
