77f6d7ea36d053818c8808a23ff805b3b515a010
Ian Remsen <ian@ianremsen.com>
2015-03-10 21:01:56 -0400

    Create api.go

---
+++ api/api.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/pmylund/go-cache"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func HandleApiSlash(w http.ResponseWriter, r *http.Request) {
+	m := Message{"_error", "Incomplete API request.", 0}
+	b, err := json.Marshal(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, "%s", b)
+}
+func HandleApiGet(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	result := r.URL.Path[9:len(r.URL.Path)]
+	gresult, found := c.Get(result)
+	if !found {
+		fmt.Fprintln(w, "Not found!")
+	} else {
+		fmt.Fprintf(w, "%s", gresult)
+	}
+}
+func HandleApiSet(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() // Parses the request body
+	key := r.PostFormValue("Key")
+	value := r.PostFormValue("Value")
+	m := Message{key, value, time.Now().UnixNano()}
+	json, err := json.Marshal(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	inum := strconv.Itoa(i)
+	c.Set(inum, json, cache.DefaultExpiration)
+	rvalue := ("entry " + inum + " with key-value pair <br>'" + m.Key + "'->'" + m.Value + "'<br>at time " + strconv.FormatInt(m.Time, 10))
+	fmt.Fprintln(w, rvalue)
+	i++
+}
