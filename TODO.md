42923f73e3a1f7725335e37fedeed59a536edb34
Ian Remsen <ian@ianremsen.com>
2015-03-15 16:10:15 -0400

    Add TODO.md

---
+++ TODO.md
@@ -0,0 +1,5 @@
+Friday TODO
+===========
+
+* SSH tunneling/nodular functionality
+* Better HTTP handling/playing nice with other web apps on the same box/port
--- friday.go
+++ friday.go
@@ -11,16 +11,19 @@ import (
 func main() {
 	dir := flag.String("d", "/var/www/gofridayadmin/", "Directory to serve from")
 	addr := flag.String("l", "0.0.0.0:7008", "Listening address")
-	r := flag.Bool("r", false, "Bool: Restore from backup")
 	file := flag.String("f", "friday.dump", "File to restore backup from and save to")
+	r := flag.Bool("r", false, "Bool: Restore from backup")
 	t := flag.Int("b", 1, "Backup frequency (in minutes, or 0 to disable)")
+
 	flag.Parse()
+
 	if *r {
 		api.Ram = backup.ReadDump(*file)
 	}
 	if *t != 0 {
 		go backup.SDump(*t, *file)
 	}
+
 	http.Handle("/", http.FileServer(http.Dir(*dir)))
 	http.HandleFunc("/api/", api.HandleApiSlash)
 	http.HandleFunc("/api/get/", api.HandleApiGet)
